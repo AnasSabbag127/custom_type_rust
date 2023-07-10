9c30ed4313007c47e5e9a890142ec73afd5b9eaa
Anas-Sabbag <anas.sabbag@remotestate.com>
2023-07-10 15:10:56 +0530

    custom type first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod structure;
+mod enums;
+use structure::struct_type;
+use crate::enums::enum_example;
+use std::env;
+
+
+fn main() {
+    println!("Hello, world!");
+    let args:Vec<String> = env::args().collect();
+    let topic_list = r#"1.structure
+2.Enum
+3.constant"#;
+    match args[1].as_str(){
+        "struct_fun" => {
+            struct_type::struct_fun();
+        },
+        "enum_fun" => {
+            enum_example::enum_fun();
+        }
+        "list" => {
+            println!(" topic_list: {:?}",topic_list);
+        },
+        _ => {
+            println!("Invalid arguement ...");
+        }
+
+    }
+
+}
